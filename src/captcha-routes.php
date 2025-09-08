cd55a158daffc98b2074f1793d13668dbbb558e1
hassan mousavi <hsmsender1370@gmail.com>
2025-09-08 14:31:48 +0330

    FEAT(middleware): add middleware for routes

--- src/captcha-routes.php
+++ src/captcha-routes.php
@@ -3,7 +3,7 @@
 use AngusDV\Captcha\Controllers\CaptchaController;
 use Illuminate\Support\Facades\Route;
 Route::as('acaptcha.')->namespace('\AngusDV\Captcha\Controllers')
-    ->prefix('acaptcha')->middleware(["web"])->group(function () {
+    ->prefix('acaptcha')->middleware(config('acaptcha.middlewares'))->group(function () {
     Route::get('generate', [CaptchaController::class,'generate']);
     Route::post('verify', [CaptchaController::class,'verify']);
     Route::post('verify/code', [CaptchaController::class,'verifyCode']);
